317715a72d7e208cff1d318da85508f293578eb3
SushiRoll53 <bryan@pif.co>
2024-04-30 14:19:04 -0600

    Place recipes in an array

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,17 @@ import Order from "../components/ordercomponent";
 
 const IndexPage: React.FC = () => {
 
-  const zt= new Recipe("ZuppaToscana", ["SpicySausage", "Bacon", "Cream", "Potatoes", "Pepper", "kale", "Onions", "Garlic", "Olive-Oil","We never use seed-oils", "Italian Dish"]);
-  const mole= new Recipe("Mole", ["Chicken", "Spices", "Chile-Pasilla", "Sesame-Seeds", "Coconut-Oil", "We never use seed-oils", "Mexican Dish"]);
-  const cr= new Recipe("Chinesse Rice", ["Rice", "Sessame-Seeds", "Butter", "Egg", "Carrots", "We never use seed-oils", "Chinesse Dish"]);
-  const steakpepper= new Recipe("Steak with Pepper", ["Steak", "Spices", "Chile-California", "Sesame-Seeds", "Coconut-Oil", "We never use seed-oils", "Mexican Dish"]);
+  const recipes: Recipe[] = [
+    new Recipe("ZuppaToscana", ["SpicySausage", "Bacon", "Cream", "Potatoes", "Pepper", "kale", "Onions", "Garlic", "Olive-Oil", "We never use seed-oils", "Italian Dish"]),
+    new Recipe("Mole", ["Chicken", "Spices", "Chile-Pasilla", "Sesame-Seeds", "Coconut-Oil", "We never use seed-oils", "Mexican Dish"]),
+    new Recipe("Chinesse Rice", ["Rice", "Sessame-Seeds", "Butter", "Egg", "Carrots", "We never use seed-oils", "Chinesse Dish"]),
+    new Recipe("Steak with Pepper", ["Steak", "Spices", "Chile-California", "Sesame-Seeds", "Coconut-Oil", "We never use seed-oils", "Mexican Dish"])
+  ];
+
+  // const zt= ;
+  // const mole= ;
+  // const cr= 
+  // const steakpepper= 
 
   // const RecipeList:Recipe[] = [];
   // RecipeList.push(zt);
@@ -36,25 +43,25 @@ const IndexPage: React.FC = () => {
         <h1>The best place to find delicious and Healthy Recipes</h1>
         <p>We have Italian, Chinesse and Mexican Plates for you to choose from</p>
       </div>
-     {/* Client Requirement Allow Users to select dishes and order from the interface
+      {/* Client Requirement Allow Users to select dishes and order from the interface
      unable to link to css global component */}
-      
-      <div style={{textAlign: "left"}}>      
-     <Order  name={zt.name} order={0}></Order>
 
-     </div>
-     <div style={{textAlign: "center"}}>      
-     <Order name={mole.name} order={0}></Order>
-     </div>
-     <div style={{textAlign: "left"}}>   
-     <Order name={cr.name} order={0}></Order>
-     </div>
-     <div style={{textAlign: "center"}}>   
-     <Order name={steakpepper.name} order={0}></Order>
-     </div>
-    {/* wanted each button to show the ingredients upon click didn't work */}
+      <div style={{ textAlign: "left" }}>
+        <Order name={recipes[0].name} order={0}></Order>
+        <OrderButton></OrderButton>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <Order name={recipes[1].name} order={0}></Order>
+      </div>
+      <div style={{ textAlign: "left" }}>
+        <Order name={recipes[2].name} order={0}></Order>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <Order name={recipes[3].name} order={0}></Order>
+      </div>
+      {/* wanted each button to show the ingredients upon click didn't work */}
       {/* {buttonLst} */}
-     {zt.ingredients}{zt.name}
+      {/* {zt.ingredients}{zt.name} */}
     </div>
   );
 };
