10cfd763d2e5d38a9c81cbda394a4e514b832713
SushiRoll53 <bryan@pif.co>
2024-04-30 14:18:35 -0600

    WIP Order button

--- components/OrderButton.tsx
+++ components/OrderButton.tsx
@@ -1,15 +1,20 @@
+import IngredientsPage from '@/pages/ingredientspage';
 import React from 'react';
-import Recipe from '../classes/Recipe';
-
 
 
 
 // import React, { FC } from 'react';
 
-const OrderButton: React.FC<Recipe> = ({name,ingredients}) => {
+const OrderButton: React.FC = () => {
+
+  function navigateToPage(){
+    // IngredientsPage()
+    // TODO: Implement navigation
+    // or Use Link 
+  }
     return (
-      <button  style={{ backgroundColor: 'black', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}>
-        {name}
+      <button onClick={navigateToPage()} style={{ backgroundColor: 'blue', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}>
+        Order
       </button>
 
     );
